d593e030edef354cbab44ee3fc85d9fbc0f86bdd
Umair Zulfiqar <umairkz52@gmail.com>
2017-06-10 17:32:26 +0500

    init

---
+++ src/routes/HeroRouter.ts
@@ -0,0 +1,64 @@
+/**
+ * Created by Umair Ahmed - 05 on 10-Jun-17.
+ */
+
+import {Router, Request, Response, NextFunction} from 'express';
+const Heroes = [{id: 1, name: 'Dom', age: 30}, {id: 2, name: 'rock', age: 45}];
+
+export class HeroRouter {
+    router: Router
+
+    /**
+     * Initialize the HeroRouter
+     */
+    constructor() {
+        this.router = Router();
+        this.init();
+    }
+
+    /**
+     * GET all Heroes.
+     */
+    public getAll(req: Request, res: Response, next: NextFunction) {
+        res.send(Heroes);
+    }
+
+    /**
+     * GET one hero by id
+     */
+    public getOne(req: Request, res: Response, next: NextFunction) {
+        let query = parseInt(req.params.id);
+        let hero = Heroes.find(hero => hero.id === query);
+        if (hero) {
+            res.status(200)
+                .send({
+                    message: 'Success',
+                    status: res.status,
+                    hero
+                });
+        }
+        else {
+            res.status(404)
+                .send({
+                    message: 'No hero found with the given id.',
+                    status: res.status
+                });
+        }
+    }
+
+    /**
+     * Take each handler, and attach to one of the Express.Router's
+     * endpoints.
+     */
+    init() {
+        this.router.get('/', this.getAll);
+        this.router.get('/:id', this.getOne);
+    }
+
+}
+
+// Create the HeroRouter, and export its configured Express.Router
+const heroRoutes = new HeroRouter();
+heroRoutes.init();
+
+export default heroRoutes.router;
